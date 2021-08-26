0c7d804c6ac254b705433d7927cd698259c01a25
Miguel <miguel.cuevas18@gmail.com>
2021-08-25 23:22:29 -0500

    added launcher logic to poki card for VaniallaJs

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
+class ProjectLauncher {
 
-window.onload = () =>{  
+  //initialize internal state
+  constructor(cardEl){
+    this.el = cardEl;
+    this.type = card.attributes.getNamedItem("name").value;
+    this.el.addEventListener('click', this.createLauncher.bind(this) );
+  }
 
-  let cardSection = document.getElementById("cards");
+  createLauncher(){
+    console.log('###  BEGIN LAUNCHING THE LAUNCHER  ###');
+    let parentEl = this.el.parentNode;
+    parentEl.innerHTML = "";
 
-  cardSection.addEventListener("click", function(event){
-    event.preventDefault();
-    event.stopPropagation();
-
-    window.location.href = "./tictactoe.html";
-
-    console.log("Fired!");
-
+    let vanillaBtn = document.createElement("div");
+    let reactBtn = document.createElement("div");
+    vanillaBtn.setAttribute("class", "launcher-btns");
+    vanillaBtn.innerText = "VanillaJS";
+    reactBtn.setAttribute("class", "launcher-btns");
+    reactBtn.innerText = "ReactJS";
 
 
+    vanillaBtn.addEventListener('click', function(e){
+      window.location.assign('./pages/pokie.html');
+    });
 
-  });
-  
-  
-  
-  
-  
-  console.log("loaded")};
 
+    parentEl.appendChild(vanillaBtn);
+    parentEl.appendChild(reactBtn);
+
+    let headerTxt = document.getElementById('cards-header');
+    headerTxt.innerHTML = '<h2> Select how to run the project: </h2>';
+  }
+
+
+
+}
+
+const cards = document.getElementsByClassName("card");
+
+for(card of cards){
+  let type = card.attributes.getNamedItem("name").value;
+
+  if (type == "pokes"){
+    console.log("initiate pokie");
+    const launcher = new ProjectLauncher(card);
+  }
+}
